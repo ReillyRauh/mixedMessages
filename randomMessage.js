0cb2618c97822999b4a864b3dfbfe1ef0f5b00cb
Reilly Rauh <rrauh@Okeene.local>
2021-11-16 14:33:43 -0600

    not work

---
+++ randomMessage.js
@@ -0,0 +1,6 @@
+const words = ['yes', 'no', 'fortnite', 'dog', 'jab', 'jim', 'world', 'earth', 'jock', 'fort', 'europe', 'america', 'middle east', 'tim buck two', 'tim buck too', 'fort', 'sumpter']
+var sentence = []
+
+function randomMessage() {
+    words.forEach(word in words)
+};
