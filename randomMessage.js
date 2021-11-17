785536691348ae3c10aeef86e9c95c6554e1cc1c
Reilly Rauh <rrauh@Okeene.local>
2021-11-17 14:06:47 -0600

    Fixed. vers#: 1.1.0

--- randomMessage.js
+++ randomMessage.js
@@ -1,6 +1,36 @@
-const words = ['yes', 'no', 'fortnite', 'dog', 'jab', 'jim', 'world', 'earth', 'jock', 'fort', 'europe', 'america', 'middle east', 'tim buck two', 'tim buck too', 'fort', 'sumpter']
-var sentence = []
+const collectWords = {
+    start: ['hi', 'ew', 'hello', 'hey', 'ugh', 'yo', 'ey', 'bro'],
+    mid: ["you're disgusting", "trash", "get good", "beautiful", "you're cute", "O_O"],
+    end: ["fortnite", "game", "bye", "send", "come on", "do it", "yesn't"]
+}
 
-function randomMessage() {
-    words.forEach(word in words)
-};
+let sentence = []
+
+function randomNumber(num) {
+    return Math.floor(Math.random() * num)
+}
+
+for (let word in collectWords) {
+    let indx = randomNumber(collectWords[word].length)
+
+    switch (word) {
+        case 'start':
+            sentence.push(collectWords[word][indx])
+            break
+        case 'mid':
+            sentence.push(collectWords[word][indx])
+            break
+        case 'end':
+            sentence.push(collectWords[word][indx])
+            break
+        default:
+            sentence.push(`No?`)
+    }
+}
+
+function formatSentence(sentence) {
+    const formattedSentence = sentence.join('\n')
+    console.log(formattedSentence)
+}
+
+formatSentence(sentence)
